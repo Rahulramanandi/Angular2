2e4529b8d902afe4e05c0661552c055bca32b1a1
Rahul Ramanandi <rahulramanandi2001@gmail.com>
2025-03-06 15:29:46 +0530

    Changes

---
+++ my-angular-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';  // Import HttpClientModule
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule  // Add HttpClientModule to imports array
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
